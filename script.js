0d21e0e80e4d8d4d77c0077bafe03d2af1ebfa60
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-08 10:56:08 -0300

    add ap

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-const search_url = 'https://musicbrainz.org/ws/2/artist/?fmt=json&query=';
-const browse_url = 'https://musicbrainz.org/ws/2/release?fmt=json&artist=';
+// const MusicBrainzApi = require('musicbrainz-api').MusicBrainzApi;
+
+// const mbApi = new MusicBrainzApi({
+//   appName: 'my-app',
+//   appVersion: '0.1.0',
+//   appContactInfo: 'user@mail.org'
+// });
+console.log("artist");
+
+const search_url = "https://musicbrainz.org/ws/2/artist/?fmt=json&query=";
+const browse_url = "https://musicbrainz.org/ws/2/release?fmt=json&artist=";
 
 // nirvana 5b11f4ce-a62d-471e-81fc-a69a8278c7da
 
 // https://musicbrainz.org/ws/2/recording?fmt=json&artist=5b11f4ce-a62d-471e-81fc-a69a8278c7da
 // https://musicbrainz.org/ws/2/release-group?artist=5b11f4ce-a62d-471e-81fc-a69a8278c7da&type=album|ep
 
+async function getArtist() {
+  const MusicBrainzApi = new require('musicbrainz-api').MusicBrainzApi;
+
+  const mbApi = new MusicBrainzApi({
+    appName: "my-app",
+    appVersion: "0.1.0",
+    appContactInfo: "user@mail.org",
+  });
+
+  const artist = await mbApi.getEntity(
+    "artist",
+    "ab2528d9-719f-4261-8098-21849222a0f2"
+  );
+
+  console.log("hi");
+  console.log(artist);
+}
+
+function searchBtnListener() {
+  console.log("listenter");
+  getArtist();
+}
+
 window.onload = () => {
-  const searchBtn = document.getElementById('search-btn');
-  searchBtn.addEventListener('click', searchBtnListener);
+  const searchBtn = document.getElementById("search-btn");
+  searchBtn.addEventListener("click", searchBtnListener);
 };
-
-function appendSearch(searchJson) {
-  const artistTitle = document.getElementById('artist-title');
-  artistTitle.innerHTML = searchJson.artists[0].name;
-}
-
-async function fetchBrowse(mbid) {
-  try {
-    const browseItem = await fetch(`${browse_url}${mbid}`);
-    const browseObj = await browseItem.json();
-    console.log(browseObj);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function fetchSearch(query) {
-  try {
-    const searchItem = await fetch(`${search_url}${query}`);
-    const searchObj = await searchItem.json();
-    console.log(searchObj);
-    appendSearch(searchObj);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-function searchBtnListener(event) {
-  const searchQuery = document.getElementById('search-input').value;
-  if (searchQuery) fetchSearch(searchQuery);
-}
