77c08a839745790bccd44e4522b08348ce31b3fe
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-13 15:56:47 -0300

    searchForm to redux

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,23 +1,52 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { addQuery } from '../../redux/actions';
 
 function SearchForm() {
   const [queryIn, setQueryIn] = useState();
+  const [entitieIn, setEntitieIn] = useState();
+  const dispatch = useDispatch();
 
-  function handleChange({target}) {
+  function handleChangeText({ target }) {
+    setQueryIn(target.value);
+  }
 
+  function handleChangeRadio({ target }) {
+    setEntitieIn(target.value);
+  }
+
+  function handleClickSearch() {
+    dispatch(addQuery(queryIn, entitieIn));
   }
 
   return (
     <form action="">
-      <input type="text" name="queryIn" id="search-input" />
+      <input
+        type="text"
+        name="queryIn"
+        value={queryIn}
+        id="search-input"
+        onChange={handleChangeText}
+      />
       <label htmlFor="album-radio">
-        <input type="radio" name="typeRadio" id="album-radio" value="album" />
+        <input
+          type="radio"
+          name="typeRadio"
+          id="album-radio"
+          value="album"
+          onChange={handleChangeRadio}
+        />
       </label>
       <label htmlFor="artist-radio">
-        <input type="radio" name="typeRadio" id="artist-radio" value="artist" />
+        <input
+          type="radio"
+          name="typeRadio"
+          id="artist-radio"
+          value="artist"
+          onChange={handleChangeRadio}
+        />
       </label>
-      <input type="button" value="Search" />
+      <input type="button" value="Search" onClick={handleClickSearch} />
     </form>
   );
 }
