a8e97d0dd6d8b6157babc374c313bb0cdfc238d4
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-10-19 18:37:14 -0300

    artistcard styling

--- src/components/ArtistCard/ArtistCard.jsx
+++ src/components/ArtistCard/ArtistCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { shape, string } from 'prop-types';
 import { Link } from 'react-router-dom';
+import StyledArtistCardLi from './ArtistCard.styled';
 
 export default function ArtistCard({ artist }) {
   return (
-    <li>
+    <StyledArtistCardLi>
       <Link to={`artist/${artist.id}`}>
         <h3>{artist.name}</h3>
         <h4>{artist.disambiguation}</h4>
         <h5>{artist.country}</h5>
-        <span>{artist.id}</span>
       </Link>
-    </li>
+    </StyledArtistCardLi>
   );
 }
 
