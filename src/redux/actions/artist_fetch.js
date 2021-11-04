14cc56468dc59fcdbee8b30ef7d966a877fd63a8
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-11-04 15:58:07 -0300

    artist serialize

--- src/redux/actions/artist_fetch.js
+++ src/redux/actions/artist_fetch.js
@@ -1,5 +1,30 @@
 import { fetchArtistAPI } from '../../services/musicBrainz';
 
+function artistSerialize(artist) {
+  const {
+    area, gender, disambiguation, id, type, name, country, isnis, genres,
+  } = artist;
+
+  return {
+    id,
+    name,
+    area,
+    disambiguation,
+    gender,
+    type,
+    country,
+    isnis,
+    genres,
+    endArea: artist['end-area'] || artist.end_area,
+    beginArea: artist['begin-area'] || artist.begin_area,
+    releaseGroups: artist['release-groups'],
+    typeId: artist['type-id'],
+    genderId: artist['gender-id'],
+    sortName: artist['sort-name'],
+    lifeSpan: artist['life-span'],
+  };
+}
+
 export const LOAD_ARTIST = 'LOAD_ARTIST';
 export const ADD_ARTIST_DATA = 'ADD_ARTIST_DATA';
 
@@ -9,6 +34,7 @@ export const loadingArtistData = (status) => ({ type: LOAD_ARTIST, status });
 export const fetchArtist = (artistMbid) => async (dispatch) => {
   try {
     const resp = await fetchArtistAPI(artistMbid);
+    console.log(artistSerialize(resp));
     return dispatch(addArtistData(resp));
   } catch (e) {
     dispatch(loadingArtistData(false));
